c17d5fcec476aab4d0205c40387ee75d58195a37
polaris <61411085+polaris428@users.noreply.github.com>
2022-07-04 18:16:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sleepingcaffeine/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.sleepingcaffeine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
