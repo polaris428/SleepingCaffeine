ecda63fdd0177ac0a90f9d8ee10b1bd5a09a5db6
polaris428 <iou1056212@naver.com>
2022-09-28 15:51:34 +0900

    Initial commit

---
+++ app/src/main/java/com/polaris04/sleepingcaffeine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.polaris04.sleepingcaffeine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
